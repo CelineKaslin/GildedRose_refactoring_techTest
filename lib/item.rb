d1015edcf5af92f8a69f579fe029130d06ab6430
celine kaslin <celine.kaslin@gmail.com>
2019-02-15 10:37:23 +0000

    rename to_s() function by item_description() in Item class, test built and passing

--- lib/item.rb
+++ lib/item.rb
@@ -1,13 +1,13 @@
 class Item
-  attr_accessor :name, :sell_in, :quality
+  attr_accessor :name, :sell_in_date, :quality
 
-  def initialize(name, sell_in, quality)
+  def initialize(name, sell_in_date, quality)
     @name = name
-    @sell_in = sell_in
+    @sell_in_date = sell_in_date
     @quality = quality
   end
 
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
+  def item_description
+    "name: #{@name}, sell in date: #{@sell_in_date} days, quality: #{@quality}"
   end
 end
---
+++ spec/item_spec.rb
@@ -0,0 +1,27 @@
+require 'item'
+
+describe Item do
+  let(:item) { described_class.new('magic potion', 10, 30)}
+
+  describe '#attribute of Item' do
+    it 'return the name of the item ' do
+      expect(item.name).to eq('magic potion')
+    end
+
+    it 'return the sell in date of the item' do
+      expect(item.sell_in_date).to eq 10
+    end
+
+    it 'return the quality of the item' do
+      expect(item.quality).to eq 30
+    end
+  end
+
+  describe '#item_description' do
+    it 'return the description of the item' do
+      expect(item.item_description).to eq('name: magic potion, sell in date: 10 days, quality: 30')
+    end
+
+  end
+
+end
